bcb77a53dc8ad5553fa0a07cbe7354d4c7269c89
Yundianxia <1257128770@qq.com>
2020-10-31 00:26:33 +0800

    Del Files

--- src/com/yuntech/encryption/Config.java
+++
@@ -1,6 +0,0 @@
-package com.yuntech.encryption;
-
-public class Config {
-    public static final String DIR_PATH = "D:\\test-watch"; //检测目录
-    public static final int POLL_TIME_OUT = 60; //已秒为单位
-}
